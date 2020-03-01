7d15048334507d785430dcb17f4f69e6c5f4cc6a
Pubudusandeepa <ppsandeepa12@gmail.com>
2020-03-01 13:25:58 +0530

    Add mat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatButtonModule } from '@angular/material/button';
 import { DefaultModule } from './layouts/default/default.module';
+import { from } from 'rxjs';
 
 
 @NgModule({
@@ -18,6 +19,7 @@ import { DefaultModule } from './layouts/default/default.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatButtonModule,
     DefaultModule
   ],
   providers: [],
--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -4,18 +4,22 @@ import { DefaultComponent } from './default.component';
 import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
 import { PostsComponent } from 'src/app/modules/posts/posts.component';
-
+import { SharedModule } from 'src/app/shared/shared.module';
+import { MatSidenavModule} from '@angular/material/sidenav';
 
 
 @NgModule({
   declarations: [
     DefaultComponent,
     DashboardComponent,
-    PostsComponent
+    PostsComponent,
+
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    SharedModule,
+    MatSidenavModule
   ]
 })
 export class DefaultModule { }
