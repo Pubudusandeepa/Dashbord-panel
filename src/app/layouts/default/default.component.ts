179ee5bb6969cb8f852194e22f2e7cfd549d09de
Pubudusandeepa <ppsandeepa12@gmail.com>
2020-03-02 09:50:48 +0530

    Event Emit

--- src/app/layouts/default/default.component.ts
+++ src/app/layouts/default/default.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DefaultComponent implements OnInit {
 
+  sideBarOpan = true;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  sideBarToggle() {
+    this.sideBarOpan = !this.sideBarOpan;
   }
 
 }
