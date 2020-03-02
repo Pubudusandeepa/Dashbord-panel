66c81bc8590bd97aceba5a31db9e01c03b604148
Pubudusandeepa <ppsandeepa12@gmail.com>
2020-03-02 12:27:29 +0530

    change aliments of chart

--- src/app/shared/widgets/card/card.component.ts
+++ src/app/shared/widgets/card/card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import HC_exporting from 'highcharts/modules/exporting';
 
 @Component({
   selector: 'app-widget-card',
@@ -7,9 +9,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  Highcharts = Highcharts;
+  chartOptions = {};
+
   constructor() { }
 
   ngOnInit(): void {
+    this.chartOptions = {
+      chart: {
+        type: 'area',
+        backgroundColor: null,
+        borderWidth: 0,
+        margin: [2, 2, 2, 2],
+        height: 60
+    },
+    title: {
+        text: null
+    },
+    subtitle: {
+        text: null
+    },
+
+    tooltip: {
+        split: true,
+        outside: true,
+    },
+    legend: {
+        enabled: false
+    },
+    credits: {
+      enabled: false,
+    },
+    exporting: {
+      enabled: false,
+    },
+
+    series: [{
+      data: [71, 69, 56, 89]
+    }]
+    };
+
+    HC_exporting(Highcharts);
+
+    setTimeout(() => {
+      window.dispatchEvent(
+        new Event('resize')
+      );
+    }, 300);
+    }
   }
 
-}
+
+
